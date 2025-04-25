74568f306f40e95249409380d47d210379a0ef3b
ouassima <elyakoubi.ouassima@gmail.com>
2025-04-25 03:23:51 +0100

    fix about me

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'aboutMe.html') // change if your file is named differently
+      }
+    }
+  }
+});
